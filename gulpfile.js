1684ef84742f02a10044fb4bfbe8ecd4b41bc19c
eugeneskom <eugeneskom@gmail.com>
2021-10-05 11:02:33 +0300

    added header, hero and greeting section

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
+// const imagemin = require('gulp-imagemin');
 const rename = require('gulp-rename');
 const del = require('del');
 const browserSync = require('browser-sync').create();
@@ -48,25 +48,26 @@ function styles() {
 }
 
 
-function images() {
-  return src('app/images/**/*.*')
-    .pipe(imagemin([
-      imagemin.gifsicle({ interlaced: true }),
-      imagemin.mozjpeg({ quality: 75, progressive: true }),
-      imagemin.optipng({ optimizationLevel: 5 }),
-      imagemin.svgo({
-        plugins: [
-          { removeViewBox: true },
-          { cleanupIDs: false }
-        ]
-      })
-    ]))
-    .pipe(dest('dist/images'))
-}
+// function images() {
+//   return src('app/images/**/*.*')
+//     .pipe(imagemin([
+//       imagemin.gifsicle({ interlaced: true }),
+//       imagemin.mozjpeg({ quality: 75, progressive: true }),
+//       imagemin.optipng({ optimizationLevel: 5 }),
+//       imagemin.svgo({
+//         plugins: [
+//           { removeViewBox: true },
+//           { cleanupIDs: false }
+//         ]
+//       })
+//     ]))
+//     .pipe(dest('dist/images'))
+// }
 
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'app/js/main.js'
 
   ])
@@ -82,7 +83,7 @@ function build() {
     'app/html/**/*.html',
     'app/css/style.min.css',
     'app/js/main.min.js',
-    'app/images/**/*.*'
+    // 'app/images/**/*.*'
   ], { base: 'app' })
     .pipe(dest('dist'))
 }
@@ -103,7 +104,8 @@ exports.styles = styles;
 exports.scripts = scripts;
 exports.browsersync = browsersync;
 exports.watching = watching;
-exports.images = images;
+// exports.images = images;
 exports.cleanDist = cleanDist;
-exports.build = series(cleanDist, images, build);
+exports.build = series(cleanDist, build); 
+// , images
 exports.default = parallel(styles, fileInclude, scripts, browsersync, watching);
