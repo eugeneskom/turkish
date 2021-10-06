3112e0b083caf0ec388adb1140603a6e5872d043
eugeneskom <eugeneskom@gmail.com>
2021-10-06 16:00:17 +0300

    first page without adaptation done

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,30 @@
-// $(function () {
-//   $('.partners__list').slick({
-//     arrows: false,
+$(function () {
+  const tabs = document.querySelector('.tabs');
+  const tabButton = document.querySelectorAll('.tabs__btn');
+  const contents = document.querySelectorAll('.tabs__page');
 
-//   });
-// })
+  tabs.onclick = e => {
+    const id = e.target.dataset.id;
+    if (id) {
+      tabButton.forEach(btn => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+
+      contents.forEach(content => {
+        content.classList.remove("active");
+      });
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  }
+
+
+//adding focus for Safari
+  document.addEventListener('click', event => {
+    if (event.target.matches('button')) {
+      event.target.focus()
+    }
+  })
+
+})
